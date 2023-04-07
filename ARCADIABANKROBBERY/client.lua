1eaf24ae6dfea86c12edd2e3d9a83e5152fa1344
Hugo Kauan Carvalho Dos Anjos <40905823+hugokauan@users.noreply.github.com>
2023-04-07 16:09:29 -0300

    Bank Robbery Resouce Update

--- ARCADIABANKROBBERY/client.lua
+++ ARCADIABANKROBBERY/client.lua
@@ -1,6 +1,16 @@
-function startRoubo(banco)
-
-end
+RegisterNetEvent('arcadia:startroubo')
+AddEventHandler('arcadia:startroubo', function()
+    local ped = PlayerPedId()
+    local animDict = "anim@heists@fleeca_bank@scope_out@return_wallet"
+    local animToPlay = "grab_cash_from_trolly"
+    while not HasAnimDictLoaded(animDict) and DoesAnimDictExist(animDict) then
+        Citizen.Wait(1)
+        RequestAnimDict(animDict)
+    end
+    if HasAnimDictLoaded(animDict) then
+        TaskPlayAnim(ped, animDict, animToPlay, 8.0, 8.0, -1, 2, 0.0, 0, 0, 1)
+    end
+end)
 
 Citizen.CreateThread(function()
     local playerPosition
--- ARCADIANTICHEAT/client.lua
+++ ARCADIANTICHEAT/client.lua
@@ -3,10 +3,8 @@ function deleteAllBlockedVehs()
     local todosVeiculos = GetGamePool('CVehicle')
     for k,veiculo in pairs(todosVeiculos) do
         veiculoHash = GetHashKey(veiculo)
-        print(k,veiculoHash)
         for _,blockedVeh in pairs(blockedVehs) do
             blockedVehHash = GetHashKey(blockedVeh)
-            print(_,blockedVehHash)
             if veiculo == blockedVehHash then
                 DeleteVehicle(veiculo)
                 TriggerServerEvent('arcadia:senddiscordmessage',"https://discord.com/api/webhooks/1086414747286654986/94Vv3nDp9uponoUSRCA36X6frJJwBpb247HVm9FlxM-HEqhIFCLlTi-h3nDIsyvD-KgM","um veículo proíbido foi detectado e deltado")
